b14b56efbfce30cc7b168729dffa337785590627
Tien Dat <126091355+TDAt2k4@users.noreply.github.com>
2025-11-17 00:06:44 +0700

    update  monitoring-prometheus-server  and  DB studentdb and table students

--- relational-database-server/init/001_init.sql
+++ relational-database-server/init/001_init.sql
@@ -1,11 +1,21 @@
-CREATE DATABASE IF NOT EXISTS minicloud;
-USE minicloud;
+-- Tạo database
+CREATE DATABASE IF NOT EXISTS studentdb;
 
-CREATE TABLE IF NOT EXISTS notes(
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  title VARCHAR(100) NOT NULL,
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+-- Chọn database
+USE studentdb;
+
+-- Tạo bảng students
+CREATE TABLE IF NOT EXISTS students (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    student_id VARCHAR(10),
+    fullname VARCHAR(100),
+    dob DATE,
+    major VARCHAR(50)
 );
 
-INSERT INTO notes(title) VALUES ('Hello from MariaDB!');
- 
+-- Chèn 3 bản ghi
+INSERT INTO students (student_id, fullname, dob, major)
+VALUES 
+    ('ST001', 'Tien Dat', '2000-01-10', 'Computer Science'),
+    ('ST002', 'Tuan Dat', '2001-02-15', 'Information Systems'),
+    ('ST003', 'Hoang Vu',   '2000-11-20', 'Software Engineering');
